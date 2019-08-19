bd3b85d7e4b9fe0efb21044d2bc39bb86ccb0bd2
Alexandre Hamez <alexandre.hamez@easymile.com>
2019-08-19 19:02:54 +0200

    Reinstate button to generate a new password

--- lib/battery_staple_web/live/password_view.ex
+++ lib/battery_staple_web/live/password_view.ex
@@ -11,8 +11,12 @@ defmodule BatteryStappleWeb.PasswordView do
         <input type="range" min="3" max="10" name="length" value="<%= @length %>" />
       </form>
       <%= @length %>
+      <div>
+        <button phx-click="gen_passwd" value="<%= @length %>">Generate new password</button>
+      </div>
     """
   end
+
 
   def mount(_session, socket) do
     Logger.debug("Socket connected: #{connected?(socket)}")
@@ -23,6 +27,11 @@ defmodule BatteryStappleWeb.PasswordView do
   def handle_event("update_length", %{"length" => length}, socket) do
     {:noreply, assign(socket, password: generate_password(length), length: length)}
   end
+
+ def handle_event("gen_passwd", value, socket) do
+    {:noreply, assign(socket, password: generate_password(value))}
+  end
+
 
   defp generate_password(length) when is_binary(length) do
     BatteryStaple.PasswordGenerator.generate_password(String.to_integer(length))

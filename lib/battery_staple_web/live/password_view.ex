f877e78b7be4e10261da75cf1e3ae17384335fdb
Alexandre Hamez <alexandre.hamez@easymile.com>
2019-08-19 15:12:01 +0200

    Use a range to choose password length

--- lib/battery_staple_web/live/password_view.ex
+++ lib/battery_staple_web/live/password_view.ex
@@ -2,26 +2,33 @@ defmodule BatteryStappleWeb.PasswordView do
   require Logger
   use Phoenix.LiveView
 
+  @initial_length 4
+
   def render(assigns) do
     ~L"""
-    <div class="">
-      <div>
-        <h1><%= @password %></h1>
-      </div>
-      <div>
-        <button phx-click="gen_passwd">Generate Password</button>
-      </div>
-    </div>
+      <%= @password %>
+      <form phx-change="update_length">
+        <input type="range" min="3" max="10" name="length" value="<%= @length %>" />
+      </form>
+      <%= @length %>
     """
   end
 
   def mount(_session, socket) do
     Logger.debug("Socket connected: #{connected?(socket)}")
 
-    {:ok, assign(socket, password: BatteryStaple.PasswordGenerator.generate_password(4))}
+    {:ok, assign(socket, password: generate_password(@initial_length), length: @initial_length)}
   end
 
-  def handle_event("gen_passwd", _value, socket) do
-    {:noreply, assign(socket, password: BatteryStaple.PasswordGenerator.generate_password(4))}
+  def handle_event("update_length", %{"length" => length}, socket) do
+    {:noreply, assign(socket, password: generate_password(length), length: length)}
+  end
+
+  defp generate_password(length) when is_binary(length) do
+    BatteryStaple.PasswordGenerator.generate_password(String.to_integer(length))
+  end
+
+  defp generate_password(length) do
+    BatteryStaple.PasswordGenerator.generate_password(length)
   end
 end
